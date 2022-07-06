cd6a198973655f388652b799fbb417de870a8e5f
Yago <yagocarvalho.dev@gmail.com>
2022-07-06 15:43:04 -0300

    alterando classname

--- src/components/Botao.tsx
+++ src/components/Botao.tsx
@@ -10,7 +10,7 @@ export default function Botao(props: BotaoProps) {
     return (
         <button onClick={props.onClick} className={`
             bg-gradient-to-r from-${cor}-400 to-${cor}-700
-            text-white px-3 py-2 rounded-md
+            text-white px-4 py-2 rounded-md
             ${props.className}
         `}>
             {props.children}
